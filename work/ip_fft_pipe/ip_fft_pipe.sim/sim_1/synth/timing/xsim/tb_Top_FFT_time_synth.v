343bff0cd3ba352ee1dcf576cf5d2057f2111d09
Hyeon Cho <mbin96@gmail.com>
2019-11-18 13:02:30 +0900

    run simul

---
+++ work/ip_fft_pipe/ip_fft_pipe.sim/sim_1/synth/timing/xsim/tb_Top_FFT_time_synth.v
@@ -0,0 +1,94 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.4.1 (win64) Build 2117270 Tue Jan 30 15:32:00 MST 2018
+// Date        : Mon Nov 18 12:51:25 2019
+// Host        : DESKTOP-KDNOE8T running 64-bit major release  (build 9200)
+// Command     : write_verilog -mode timesim -nolib -sdf_anno true -force -file
+//               C:/Users/mbin9/Documents/19-2-project/19_2_soc_design/work/ip_fft_pipe/ip_fft_pipe.sim/sim_1/synth/timing/xsim/tb_Top_FFT_time_synth.v
+// Design      : tb_Top_FFT
+// Purpose     : This verilog netlist is a timing simulation representation of the design and should not be modified or
+//               synthesized. Please ensure that this netlist is used with the corresponding SDF file.
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+`define XIL_TIMING
+
+(* NotValidForBitStream *)
+module tb_Top_FFT
+   ();
+
+
+initial begin
+ $sdf_annotate("tb_Top_FFT_time_synth.sdf",,,,"tool_control");
+end
+endmodule
+`ifndef GLBL
+`define GLBL
+`timescale  1 ps / 1 ps
+
+module glbl ();
+
+    parameter ROC_WIDTH = 100000;
+    parameter TOC_WIDTH = 0;
+
+//--------   STARTUP Globals --------------
+    wire GSR;
+    wire GTS;
+    wire GWE;
+    wire PRLD;
+    tri1 p_up_tmp;
+    tri (weak1, strong0) PLL_LOCKG = p_up_tmp;
+
+    wire PROGB_GLBL;
+    wire CCLKO_GLBL;
+    wire FCSBO_GLBL;
+    wire [3:0] DO_GLBL;
+    wire [3:0] DI_GLBL;
+   
+    reg GSR_int;
+    reg GTS_int;
+    reg PRLD_int;
+
+//--------   JTAG Globals --------------
+    wire JTAG_TDO_GLBL;
+    wire JTAG_TCK_GLBL;
+    wire JTAG_TDI_GLBL;
+    wire JTAG_TMS_GLBL;
+    wire JTAG_TRST_GLBL;
+
+    reg JTAG_CAPTURE_GLBL;
+    reg JTAG_RESET_GLBL;
+    reg JTAG_SHIFT_GLBL;
+    reg JTAG_UPDATE_GLBL;
+    reg JTAG_RUNTEST_GLBL;
+
+    reg JTAG_SEL1_GLBL = 0;
+    reg JTAG_SEL2_GLBL = 0 ;
+    reg JTAG_SEL3_GLBL = 0;
+    reg JTAG_SEL4_GLBL = 0;
+
+    reg JTAG_USER_TDO1_GLBL = 1'bz;
+    reg JTAG_USER_TDO2_GLBL = 1'bz;
+    reg JTAG_USER_TDO3_GLBL = 1'bz;
+    reg JTAG_USER_TDO4_GLBL = 1'bz;
+
+    assign (strong1, weak0) GSR = GSR_int;
+    assign (strong1, weak0) GTS = GTS_int;
+    assign (weak1, weak0) PRLD = PRLD_int;
+
+    initial begin
+	GSR_int = 1'b1;
+	PRLD_int = 1'b1;
+	#(ROC_WIDTH)
+	GSR_int = 1'b0;
+	PRLD_int = 1'b0;
+    end
+
+    initial begin
+	GTS_int = 1'b1;
+	#(TOC_WIDTH)
+	GTS_int = 1'b0;
+    end
+
+endmodule
+`endif
---
+++ work/ip_fft_pipe/ip_fft_pipe.srcs/sim_1/imports/Lab_SD2019_2_prob/tb_Top_FFT.v
@@ -0,0 +1,71 @@
+`timescale 1ns/1ps
+`define p 20
+module tb_Top_FFT;
+
+reg nrst,clk;
+
+
+reg [15:0] input_re[191:0];
+reg [15:0] input_im[191:0];
+
+reg [15:0] output_re[191:0];
+reg [15:0] output_im[191:0];
+
+wire [15:0] inReal,inImag;
+wire [15:0] outReal,outImag;
+
+integer clkcnt;
+
+reg start;
+
+
+Top_FFT top0(.nrst(nrst),.clk(clk),.start(start),.valid(1'b1),.inReal(inReal),.inImag(inImag),.outReal(outReal),.outImag(outImag));
+
+always
+  #(`p/2)  clk = !clk;
+ 
+always@(negedge clk)
+  clkcnt = clkcnt +1;
+
+initial begin
+  clk  = 0;
+  nrst = 0;
+  clkcnt=-103;
+  start = 0;
+  $readmemb("binary_in_real.txt", input_re);
+  $readmemb("binary_in_imag.txt", input_im);
+
+  #(100*`p) start = 1'b1; 
+  #(`p/2+1) nrst = 1;
+end
+
+assign inReal = clkcnt > -2? input_re[clkcnt+1] : 0;
+assign inImag = clkcnt > -2? input_im[clkcnt+1] : 0;
+
+always @ (posedge clk) begin
+output_re[clkcnt+1] <= outReal;
+output_im[clkcnt+1] <= outImag;
+end
+
+
+integer dumpfile, i;
+initial begin
+
+
+  #(292*`p +1) dumpfile = $fopen("binary_out_real.txt","w");
+  for(i = 0; i<192;i=i+1)begin
+  $fwrite(dumpfile,"%b\n",output_re[i]);
+  end
+  $fclose(dumpfile);
+  
+    dumpfile = $fopen("binary_out_imag.txt","w");
+    for(i = 0; i<192;i=i+1)begin
+    $fwrite(dumpfile,"%b\n",output_im[i]);
+    end
+    $fclose(dumpfile);
+
+  $stop;
+end
+
+
+endmodule
